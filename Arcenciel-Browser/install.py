6b081cbe777ae860ddf4c0de9000b82b40b16eb5
Anzhc <133806049+Anzhc@users.noreply.github.com>
2025-03-14 04:27:00 +0400

    Delete Arcenciel-Browser directory

--- Arcenciel-Browser/install.py
+++
@@ -1,6 +0,0 @@
-# /a1111/extensions/Arcenciel/install.py
-import launch
-
-# We'll use 'requests' for Arcenciel API calls.
-if not launch.is_installed("requests"):
-    launch.run_pip("install requests", "requirements for Arcenciel extension")
